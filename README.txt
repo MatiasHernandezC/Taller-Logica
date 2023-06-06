771fad36f5dbfe9f5d92b770f7040c963fae4a34
Matias <matias.hernandez.ca@usach.cl>
2023-06-06 00:51:48 -0400

    Actividad fisica terminado

--- README.txt
+++ README.txt
@@ -7,3 +7,15 @@ Instalacion de pyswipl:
   - Instalar pip3 para poder instalar la libreria de prolog en python
   - Insertar en CMD: "pip3 install pyswipl"
 Con lo anterior ya se podria correr el programa
+
+Links logica de prolog:
+  Screen time:
+  - https://www.osfhealthcare.org/blog/kids-screen-time-how-much-is-too-much/
+  - https://www.verywellfamily.com/the-negative-effects-of-too-much-screen-time-1094877
+  - https://www.reidhealth.org/blog/screen-time-for-adults
+  Horas sueño:
+  - https://www.mayoclinic.org/es-es/healthy-lifestyle/adult-health/expert-answers/how-many-hours-of-sleep-are-enough/faq-20057898
+  Actividad fisica
+  - https://www.who.int/es/news/item/25-11-2020-every-move-counts-towards-better-health-says-who
+  - https://www.unicef.org/uruguay/media/2276/file/La%20actividad%20f%C3%ADsica%20en%20ni%C3%B1os,%20ni%C3%B1as%20y%20adolescentes.pdf
+
--- src/consultas.pl
+++ src/consultas.pl
@@ -12,6 +12,7 @@ edad(X, adulto_joven):-
     X < 26.
 edad(X, adulto):-
     X >= 26.
+% -------------------------------------------------------------------------
 % Calculo de maximo screen time
 % Screen time:
 %  - https://www.osfhealthcare.org/blog/kids-screen-time-how-much-is-too-much/
@@ -31,14 +32,14 @@ estado_screen_time(X, Y, Z) :-
 % Poco Saludable
 estado_screen_time(X, Y, Z) :-
     max_screen_time(X, Max),
-    Y = Max,
+    (Y = Max;Y = Max+1),
     Z = 2.
 % No saludable
 estado_screen_time(X, Y, Z) :-
     max_screen_time(X, Max),
     Y > Max,
     Z = 3.
-
+% -------------------------------------------------------------------------
 % Calculo de min de horas de sueño
 % Bebés de 4 a 12 meses 	De 12 a 16 horas por cada 24 horas, incluidas las siestas
 % De 1 a 2 años 	        De 11 a 14 horas por cada 24 horas, incluidas las siestas
@@ -53,6 +54,7 @@ horas_sueño_min(niño, 10).
 horas_sueño_min(adolecente, 9).
 horas_sueño_min(adulto_joven, 7).
 horas_sueño_min(adulto, 7).
+% Calculo de saludabilidad ((1: saludable, 2: poco saludable, 3 o mas: no saludable))
 % Saludable
 estado_horas_sueño(X, Y, Z) :-
     horas_sueño_min(X, Max),
@@ -70,32 +72,60 @@ estado_horas_sueño(X, Y, Z) :-
     horas_sueño_min(X, Max),
     Y < Max+1,
     Z = 3.
-
+% -------------------------------------------------------------------------
 % Drogas y alcohol
 consume(nada, alcohol, 0).
 consume(poco, alcohol, 0).
 consume(mucho, alcohol, 1).
-consume(demaciado, alcohol, 3).
+consume(demasiado, alcohol, 3).
 
 consume(nada, tabaco, 0).
 consume(poco, tabaco, 1).
 consume(mucho, tabaco, 2).
-consume(demaciado, tabaco, 2).
+consume(demasiado, tabaco, 2).
 
 consume(nada, drogas, 0).
 consume(poco, drogas, 1).
 consume(mucho, drogas, 2).
-consume(demaciado, drogas, 2).
+consume(demasiado, drogas, 3).
 
-% Saludable
+% Calculo de saludabilidad ((1: saludable, 2: poco saludable, 3 o mas: no saludable))
 consumo_estupefacientes([], [], 0).
 consumo_estupefacientes([H1|T1], [H2|T2], Z) :-
     consume(H1, H2, Cont),
     consumo_estupefacientes(T1, T2, Z1),
-    Z = Z1+Cont.
+    Z = Z1 + Cont.
+% -------------------------------------------------------------------------
+% Actividad fisica
+% https://www.who.int/es/news/item/25-11-2020-every-move-counts-towards-better-health-says-who
+% Las nuevas directrices recomiendan por lo menos de 150 a 300 minutos de 
+% actividad física aeróbica de intensidad moderada o vigorosa por semana 
+% para todos los adultos, incluidas las personas que viven con afecciones 
+% crónicas o discapacidad, y un promedio de 60 minutos al día para los niños 
+% y adolescentes.
+%
+% https://www.unicef.org/uruguay/media/2276/file/La%20actividad%20f%C3%ADsica%20en%20ni%C3%B1os,%20ni%C3%B1as%20y%20adolescentes.pdf
+% 0 a 2 año: 0 de actividad física de intensidad moderada a vigorosa
+% 2 a 4 años: 60 minutos son de actividad física de intensidad moderada a vigorosa repartidas en el dia
+% 5 y 17 años: 60 minutos de actividad física de intensidad moderada a vigorosa diariamente.
+% 
+% 
+actividad_fisica(bebe, poco, 2).
+actividad_fisica(bebe, normal, 1).
+actividad_fisica(bebe, mucho, 2).
 
-% Asumir que:
-%   - 0 - 33: Saludable
-%   - 34 - 66: Poco saludable
-%   - 67 - 100: No saludable
+actividad_fisica(niño, poco, 2).
+actividad_fisica(niño, normal, 1).
+actividad_fisica(niño, mucho, 1).
 
+actividad_fisica(adolecente, poco, 3).
+actividad_fisica(adolecente, normal, 2).
+actividad_fisica(adolecente, mucho, 1).
+
+actividad_fisica(adulto_joven, poco, 3).
+actividad_fisica(adulto_joven, normal, 2).
+actividad_fisica(adulto_joven, mucho, 1).
+
+actividad_fisica(adulto, poco, 3).
+actividad_fisica(adulto, normal, 2).
+actividad_fisica(adulto, mucho, 1).
